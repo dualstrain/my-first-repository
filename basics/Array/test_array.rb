eee79a5a940e3428d34a0c5e7813e7f23b3f8b00
Dustin Leung <dualstrain@gmail.com>
2015-02-03 00:28:35 -0500

    Array fin.

--- basics/Array/test_array.rb
+++ basics/Array/test_array.rb
@@ -11,3 +11,14 @@ end
 
 arr.include?(number)
 	puts "#{number} is very well in the array."
+
+arr = [["test", "hello", "world"], ["example", "mem"]]
+puts arr.last.first
+
+arr = [1,2,3,4]
+arr2 = []
+arr.each do |n|
+		arr2.push(n + 2)
+	end
+p arr
+p arr2
