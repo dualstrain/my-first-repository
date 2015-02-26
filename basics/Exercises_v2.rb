8b66c4ef831c16a700e6e40954088aba36eeecb6
dualstrain <dualstrain@gmail.com>
2015-02-26 17:24:00 -0500

    Exercises_pt1

---
+++ basics/Exercises_v2.rb
@@ -0,0 +1,32 @@
+# quizzes
+#5
+ans = (10..100).cover?(42) 
+puts ans
+
+#6
+str = "and seven years ago..."
+str.prepend("Four score and ")
+puts str
+
+#8
+arr = ["Fred", "Wilma"]
+arr << ["Barney", "Betty"]
+arr << ["BamBam", "Pebbles"]
+
+p arr.flatten()
+p arr
+
+#9
+arr_hash = {"Fred"=>0, "Wilma"=>1, "Barney"=>2, "Betty"=>3, "BamBam"=>4, "Pebbles"=>5}
+arr = arr_hash.assoc("Barney")
+p arr
+
+#10
+arr = ["Fred", "Barney", "Wilma", "Betty", "Pebbles", "BamBam"]
+arrHash = {}
+arr.each_with_index do | v,index |
+	arrHash[v] = index 
+end
+p arrHash
+
+# shall fall
